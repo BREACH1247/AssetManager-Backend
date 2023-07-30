0588bb99ee05c55cb299348f350537b4e3ce583f
BREACH1247 <sinhaditya50@gmail.com>
2023-07-30 17:37:42 +0530

    server 2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const maintenanceRecordRoutes = require("./Routes/maintainenceRecord");
 const assetSchedulingRoutes = require("./Routes/assetScheduling");
 const assetSpecRoutes = require("./Routes/assetSpecification");
 const getAssetRoutes = require("./Routes/getAssets.js")
-
+const AssetSpecification = require("./Models/AssetSpecification")
 var uri = "mongodb://anyone:1234@ac-hceg3j7-shard-00-00.8f2jhii.mongodb.net:27017,ac-hceg3j7-shard-00-01.8f2jhii.mongodb.net:27017,ac-hceg3j7-shard-00-02.8f2jhii.mongodb.net:27017/?ssl=true&replicaSet=atlas-10esad-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.set("strictQuery", false);
 
@@ -27,4 +27,16 @@ mongoose.connect(uri).then(()=>{
 app.use("/recordCreation", maintenanceRecordRoutes);
 app.use("/scheduleCreation", assetSchedulingRoutes);
 app.use("/assetCreation", assetSpecRoutes);
-app.use("/getAsset", getAssetRoutes);
+app.get("/getAsset", async(req,res) => {
+  console.log("Triggering")
+  if(req.body != {}){
+    const qassets = await AssetSpecification.where("name").equals(req.body.name).where("buildingType").equals(req.body.buiding);
+    console.log(qassets);
+    res.status(200).json(qassets);
+  }
+  else{
+    const allassets = await AssetSpecification.find();
+    console.log(allassets);
+    res.status(200).json(allassets);
+  }
+});
